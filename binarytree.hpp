711887bbfcc2f247cc130484586d5395c17fd828
Frank Lu <38390471+FrankLuuuu@users.noreply.github.com>
2024-05-20 16:49:52 -0500

    Add files via upload

---
+++ binarytree.hpp
@@ -0,0 +1,157 @@
+/**
+ * @file binarytree.hpp
+ * Definitions of the binary tree functions you'll be writing for this lab.
+ * You'll need to modify this file.
+ */
+#include "InorderTraversal.h"
+#include <iostream>
+#include <vector>
+
+/**
+ * @return The height of the binary tree. Recall that the height of a binary
+ *  tree is just the length of the longest path from the root to a leaf, and
+ *  that the height of an empty tree is -1.
+ */
+template <typename T>
+int BinaryTree<T>::height() const
+{
+    // Call recursive helper function on root
+    return height(root);
+}
+
+/**
+ * Private helper function for the public height function.
+ * @param subRoot
+ * @return The height of the subtree
+ */
+template <typename T>
+int BinaryTree<T>::height(const Node* subRoot) const
+{
+    // Base case
+    if (subRoot == NULL)
+        return -1;
+
+    // Recursive definition
+    return 1 + std::max(height(subRoot->left), height(subRoot->right));
+}
+
+/**
+ * Prints out the values of the nodes of a binary tree in order.
+ * That is, everything to the left of a node will be printed out before that
+ * node itself, and everything to the right of a node will be printed out after
+ * that node.
+ */
+template <typename T>
+void BinaryTree<T>::printLeftToRight() const
+{
+    // Call recursive helper function on the root
+    printLeftToRight(root);
+
+    // Finish the line
+    std::cout << std::endl;
+}
+
+/**
+ * Private helper function for the public printLeftToRight function.
+ * @param subRoot
+ */
+template <typename T>
+void BinaryTree<T>::printLeftToRight(const Node* subRoot) const
+{
+    // Base case - null node
+    if (subRoot == NULL)
+        return;
+
+    // Print left subtree
+    printLeftToRight(subRoot->left);
+
+    // Print this node
+    std::cout << subRoot->elem << ' ';
+
+    // Print right subtree
+    printLeftToRight(subRoot->right);
+}
+
+/**
+ * Flips the tree over a vertical axis, modifying the tree itself
+ *  (not creating a flipped copy).
+ */
+    template <typename T>
+void BinaryTree<T>::mirror()
+{
+    //your code here
+    mirror(root);
+}
+
+template <typename T>
+void BinaryTree<T>::mirror(Node* node) {
+    if(node == NULL)
+        return;
+
+    mirror(node->left);
+    mirror(node->right);
+    
+    Node * temp = node->right;
+    node->right = node->left;
+    node->left = temp;
+}
+
+/**
+ * isOrdered() function iterative version
+ * @return True if an in-order traversal of the tree would produce a
+ *  nondecreasing list output values, and false otherwise. This is also the
+ *  criterion for a binary tree to be a binary search tree.
+ */
+template <typename T>
+bool BinaryTree<T>::isOrderedIterative() const
+{
+    InorderTraversal<T> inOrder(root);
+    bool ordered = true;
+    T temp;
+    bool first = true;
+
+    for (typename TreeTraversal<T>::Iterator iterator = inOrder.begin(); iterator != inOrder.end(); ++iterator) {
+        if(!first)
+            if((*iterator)->elem <= temp)
+                ordered = false;
+
+        temp = (*iterator)->elem;
+        first = false;
+    }
+
+    return ordered;
+}
+
+/**
+ * isOrdered() function recursive version
+ * @return True if an in-order traversal of the tree would produce a
+ *  nondecreasing list output values, and false otherwise. This is also the
+ *  criterion for a binary tree to be a binary search tree.
+ */
+template <typename T>
+bool BinaryTree<T>::isOrderedRecursive() const
+{
+    // your code here
+    std::vector<T> v;
+    isOrderedRecursive(root, v);
+
+    bool ordered = true; 
+    for(unsigned long i = 0; i < v.size() - 1; i++){
+        if(v[i] >= v[i+1]){
+            ordered = false;
+        }
+    }
+    
+    return ordered;
+}
+
+template <typename T>
+void BinaryTree<T>::isOrderedRecursive(const Node* subRoot, std::vector<T> &v) const {
+    if(subRoot == NULL)
+        return;
+
+    isOrderedRecursive(subRoot->left, v);
+    v.push_back(subRoot->elem);
+    isOrderedRecursive(subRoot->right, v);
+}
+
